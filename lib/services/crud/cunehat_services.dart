18e09570ff69fce442014049d779dbb8aeecd66d
halil ibrahim avsar <hllbrhmavsar@gmail.com>
2023-05-15 03:40:42 +0300

    added functions inside CunehatServices for expense, income and user fields

--- lib/services/crud/cunehat_services.dart
+++ lib/services/crud/cunehat_services.dart
@@ -1,4 +1,5 @@
 import 'package:cunehat/services/crud/database_exceptions.dart';
+import 'package:flutter/foundation.dart';
 import 'package:sqflite/sqflite.dart';
 import 'package:path/path.dart' show join;
 import 'package:path_provider/path_provider.dart';
@@ -8,13 +9,16 @@ const dbName = "cunehat.db";
 
 // Table names
 const userTable = "users";
-const dataTable = "data";
+const expenseTable = "expense";
+const incomeTable = "income";
 
 // For the field of the user table
 const idColmn = "id";
 const emailColmn = "email";
 
-// For the field of the data table
+// Below variables is the same for both Expense and Income databases.
+// The table title will be same but content(data inside of the tables)
+// will be different.
 const userIdColmn = "user_id";
 const priceColmn = "price";
 const noteColmn = "note";
@@ -23,28 +27,64 @@ const dateColmn = "date";
 const timeColmn = "time";
 const isSyncedWithCloudColmn = "is_synced_with_cloud";
 
-// create tables in sql syntax
-const createDataTable = '''CREATE TABLE IF NOT EXISTS "data" (
-                          "id"	INTEGER NOT NULL UNIQUE,
-                          "price"	REAL,
-                          "note"	TEXT,
-                          "tag"	TEXT,
-                          "date"	TEXT,
-                          "time"	TEXT,
-                          "user_id"	INTEGER NOT NULL,
-                          "is_synced_with_cloud"	INTEGER NOT NULL DEFAULT 0,
-                          PRIMARY KEY("id" AUTOINCREMENT),
-                          FOREIGN KEY("user_id") REFERENCES "users"("id")
-                        );''';
-
 const createUserTable = '''CREATE TABLE IF NOT EXISTS "users" (
                           "id"	INTEGER NOT NULL UNIQUE,
                           "email"	TEXT NOT NULL UNIQUE,
                           PRIMARY KEY("id" AUTOINCREMENT)
                         );''';
 
+// create tables in sql syntax
+const createExpenseTable = '''CREATE TABLE IF NOT EXISTS "$expenseTable" (
+                          "id"	INTEGER NOT NULL UNIQUE,
+                          "$priceColmn"	REAL,
+                          "$noteColmn"	TEXT,
+                          "$tagColmn"	TEXT,
+                          "$dateColmn"	TEXT,
+                          "$timeColmn"	TEXT,
+                          "$userIdColmn"	INTEGER NOT NULL,
+                          "$isSyncedWithCloudColmn"	INTEGER NOT NULL DEFAULT 0,
+                          PRIMARY KEY("id" AUTOINCREMENT),
+                          FOREIGN KEY("$userIdColmn") REFERENCES "$userTable"("id")
+                        );''';
+
+const createIncomeTable = '''CREATE TABLE IF NOT EXISTS "$incomeTable" (
+                          "id"	INTEGER NOT NULL UNIQUE,
+                          "$priceColmn"	REAL,
+                          "$noteColmn"	TEXT,
+                          "$tagColmn"	TEXT,
+                          "$dateColmn"	TEXT,
+                          "$timeColmn"	TEXT,
+                          "$userIdColmn"	INTEGER NOT NULL,
+                          "$isSyncedWithCloudColmn"	INTEGER NOT NULL DEFAULT 0,
+                          PRIMARY KEY("id" AUTOINCREMENT),
+                          FOREIGN KEY("$userIdColmn") REFERENCES "$userTable"("id")
+                        );''';
+
 class CunehatServices {
   Database? _db;
+  Database _getDatabaseOrThrow() {
+    final db = _db;
+    if (db == null) {
+      throw DatabaseIsNotOpen();
+    } else {
+      return db;
+    }
+  }
+
+  Future<void> _ensureDbIsOpen() async {
+    try {
+      await open();
+    } on DatabaseAlreadyOpenException {
+      // empty
+    }
+  }
+
+  Future<void> close() async {
+    final db = _getDatabaseOrThrow();
+
+    await db.close();
+    _db = null;
+  }
 
   Future<void> open() async {
     // if database is opened throw an error
@@ -58,27 +98,320 @@ class CunehatServices {
       _db = db; // assign to our private variable
       // create tables
       await db.execute(createUserTable);
-      await db.execute(createDataTable);
+      await db.execute(createExpenseTable);
+      await db.execute(createIncomeTable);
     } on MissingPlatformDirectoryException {
       throw UnableToGetDirectoryException;
     }
   }
 
-  Future<void> close() async {
-    // if (_db != null) {
-    //   throw DatabaseAlreadyOpenException();
-    // }
-    // try {
-    //   await _db.close();
-    // }
+  // user services
+  Future<DatabaseUser> userCreate({required String email}) async {
+    await _ensureDbIsOpen();
+    final db = _getDatabaseOrThrow();
+    final results = await db.query(
+      userTable,
+      limit: 1,
+      where: 'email = ?',
+      whereArgs: [email.toLowerCase()],
+    );
+    if (results.isNotEmpty) {
+      throw UserAlreadyExists();
+    }
+
+    final userId = await db.insert(userTable, {
+      emailColmn: email.toLowerCase(),
+    });
+
+    return DatabaseUser(
+      id: userId,
+      email: email,
+    );
+  }
+
+  Future<DatabaseUser> userGet({required String email}) async {
+    await _ensureDbIsOpen();
+    final db = _getDatabaseOrThrow();
+
+    final results = await db.query(
+      userTable,
+      limit: 1,
+      where: 'email = ?',
+      whereArgs: [email.toLowerCase()],
+    );
+
+    if (results.isEmpty) {
+      throw CouldNotFindUser();
+    } else {
+      return DatabaseUser.fromRow(results.first);
+    }
+  }
+
+  Future<void> userDelete({required String email}) async {
+    await _ensureDbIsOpen();
+    final db = _getDatabaseOrThrow();
+    final deletedCount = await db.delete(
+      userTable,
+      where: 'email = ?',
+      whereArgs: [email.toLowerCase()],
+    );
+    if (deletedCount != 1) {
+      throw CouldNotDeleteUser();
+    }
+  }
+
+  //expense services
+  Future<DbExpense> expenseCreate({
+    required DatabaseUser owner,
+    required double price,
+    required String note,
+    required String tag,
+    required String date,
+    required String time,
+    required bool isSynecWithCloud,
+  }) async {
+    await _ensureDbIsOpen();
+    final db = _getDatabaseOrThrow();
+
+    // make sure owner exists in the database with the correct id
+    final dbUser = await userGet(email: owner.email);
+    if (dbUser != owner) {
+      throw CouldNotFindUser();
+    }
+
+    // create Expense
+    final expenseId = await db.insert(expenseTable, {
+      priceColmn: price,
+      noteColmn: note,
+      tagColmn: tag,
+      dateColmn: date,
+      timeColmn: time,
+      userIdColmn: owner.id,
+    });
+
+    final expense = DbExpense(
+      id: expenseId,
+      price: price,
+      note: note,
+      tag: tag,
+      date: date,
+      time: time,
+      isSyncedWithCloud: isSynecWithCloud,
+      userId: owner.id,
+    );
+
+    // _notes.add(expense);
+    // _notesStreamController.add(expense);
+
+    return expense;
+  }
+
+  Future<void> expenseDelete({required int id}) async {
+    await _ensureDbIsOpen();
+    final db = _getDatabaseOrThrow();
+    final deletedCount = await db.delete(
+      expenseTable,
+      where: 'id = ?',
+      whereArgs: [id],
+    );
+    if (deletedCount == 0) {
+      throw CouldNotDeleteData();
+    } else {
+      // _notes.removeWhere((note) => note.id == id);
+      // _notesStreamController.add(_notes);
+    }
+  }
+
+  Future<DbExpense> expenseGet({required int id}) async {
+    await _ensureDbIsOpen();
+    final db = _getDatabaseOrThrow();
+    final expenses = await db.query(
+      expenseTable,
+      limit: 1,
+      where: 'id = ?',
+      whereArgs: [id],
+    );
+
+    if (expenses.isEmpty) {
+      throw CouldNotFindData();
+    } else {
+      final expense = DbExpense.fromRow(expenses.first);
+      // _notes.removeWhere((note) => note.id == id);
+      // _notes.add(note);
+      // _notesStreamController.add(_notes);
+      return expense;
+    }
+  }
+
+  Future<DbExpense> expenseUpdate({
+    required DbExpense expense,
+    required double price,
+    required String note,
+    required String tag,
+    required String date,
+    required String time,
+    required bool isSynecWithCloud,
+  }) async {
+    await _ensureDbIsOpen();
+    final db = _getDatabaseOrThrow();
+
+    // make sure note exists
+    await expenseGet(id: expense.id);
+
+    // update DB
+    final updatesCount = await db.update(
+      expenseTable,
+      {
+        priceColmn: price,
+        noteColmn: note,
+        tagColmn: tag,
+        dateColmn: date,
+        timeColmn: time,
+      },
+      where: 'id = ?',
+      whereArgs: [expense.id],
+    );
+
+    if (updatesCount == 0) {
+      throw CouldNotUpdateData();
+    } else {
+      final updatedExpense = await expenseGet(id: expense.id);
+      // _notes.removeWhere((note) => note.id == updatedNote.id);
+      // _notes.add(updatedNote);
+      // _notesStreamController.add(_notes);
+      return updatedExpense;
+    }
+  }
+
+  // income service
+  Future<DbIncome> incomeCreate({
+    required DatabaseUser owner,
+    required double price,
+    required String note,
+    required String tag,
+    required String date,
+    required String time,
+    required bool isSynecWithCloud,
+  }) async {
+    await _ensureDbIsOpen();
+    final db = _getDatabaseOrThrow();
+
+    // make sure owner exists in the database with the correct id
+    final dbUser = await userGet(email: owner.email);
+    if (dbUser != owner) {
+      throw CouldNotFindUser();
+    }
+
+    final incomeId = await db.insert(incomeTable, {
+      priceColmn: price,
+      noteColmn: note,
+      tagColmn: tag,
+      dateColmn: date,
+      timeColmn: time,
+      userIdColmn: owner.id,
+    });
+
+    final income = DbIncome(
+      id: incomeId,
+      price: price,
+      note: note,
+      tag: tag,
+      date: date,
+      time: time,
+      isSyncedWithCloud: isSynecWithCloud,
+      userId: owner.id,
+    );
+
+    // _notes.add(expense);
+    // _notesStreamController.add(expense);
+
+    return income;
+  }
+
+  Future<void> incomeDelete({required int id}) async {
+    await _ensureDbIsOpen();
+    final db = _getDatabaseOrThrow();
+    final deletedCount = await db.delete(
+      incomeTable,
+      where: 'id = ?',
+      whereArgs: [id],
+    );
+    if (deletedCount == 0) {
+      throw CouldNotDeleteData();
+    } else {
+      // _notes.removeWhere((note) => note.id == id);
+      // _notesStreamController.add(_notes);
+    }
+  }
+
+  Future<DbIncome> incomeGet({required int id}) async {
+    await _ensureDbIsOpen();
+    final db = _getDatabaseOrThrow();
+    final incomes = await db.query(
+      incomeTable,
+      limit: 1,
+      where: 'id = ?',
+      whereArgs: [id],
+    );
+
+    if (incomes.isEmpty) {
+      throw CouldNotFindData();
+    } else {
+      final income = DbIncome.fromRow(incomes.first);
+      // _notes.removeWhere((note) => note.id == id);
+      // _notes.add(note);
+      // _notesStreamController.add(_notes);
+      return income;
+    }
+  }
+
+  Future<DbIncome> incomeUpdate({
+    required DbIncome income,
+    required double price,
+    required String note,
+    required String tag,
+    required String date,
+    required String time,
+    required bool isSynecWithCloud,
+  }) async {
+    await _ensureDbIsOpen();
+    final db = _getDatabaseOrThrow();
+
+    // make sure note exists
+    await incomeGet(id: income.id);
+
+    // update DB
+    final updatesCount = await db.update(
+      incomeTable,
+      {
+        priceColmn: price,
+        noteColmn: note,
+        tagColmn: tag,
+        dateColmn: date,
+        timeColmn: time,
+      },
+      where: 'id = ?',
+      whereArgs: [income.id],
+    );
+
+    if (updatesCount == 0) {
+      throw CouldNotUpdateData();
+    } else {
+      final updatedIncome = await incomeGet(id: income.id);
+      // _notes.removeWhere((note) => note.id == updatedNote.id);
+      // _notes.add(updatedNote);
+      // _notesStreamController.add(_notes);
+      return updatedIncome;
+    }
   }
 }
 
+@immutable
 class DatabaseUser {
   final int id;
   final String email;
 
-  DatabaseUser({required this.id, required this.email});
+  const DatabaseUser({required this.id, required this.email});
 
   DatabaseUser.fromRow(Map<String, Object?> map)
       : id = map[idColmn] as int,
@@ -103,7 +436,7 @@ class DatabaseUser {
   int get hashCode => id.hashCode;
 }
 
-class DatabaseCunehat {
+class DbExpense {
   final int id;
   final double price;
   final String note;
@@ -113,7 +446,7 @@ class DatabaseCunehat {
   final int userId;
   final bool isSyncedWithCloud;
 
-  DatabaseCunehat({
+  DbExpense({
     required this.id,
     required this.price,
     required this.note,
@@ -124,7 +457,54 @@ class DatabaseCunehat {
     required this.isSyncedWithCloud,
   });
 
-  DatabaseCunehat.fromRow(Map<String, Object?> map)
+  DbExpense.fromRow(Map<String, Object?> map)
+      : id = map[idColmn] as int,
+        price = map[priceColmn] as double,
+        note = map[noteColmn] as String,
+        tag = map[tagColmn] as String,
+        date = map[dateColmn] as String,
+        time = map[timeColmn] as String,
+        userId = map[userIdColmn] as int,
+        isSyncedWithCloud =
+            (map[isSyncedWithCloudColmn] as int) == 1 ? true : false;
+
+  @override
+  String toString() {
+    return 'id : $id \nprice : $price \nnote : $note \ntag : $tag \ndate : $date \ntime : $time \nuserId : $userId \nisSyncedWithCloud : $isSyncedWithCloud \n';
+  }
+
+  @override
+  operator ==(covariant DbExpense other) {
+    return id == other.id;
+  }
+
+  // Below code is also part of the overriding `==` operator.
+  @override
+  int get hashCode => id.hashCode;
+}
+
+class DbIncome {
+  final int id;
+  final double price;
+  final String note;
+  final String tag;
+  final String date;
+  final String time;
+  final int userId;
+  final bool isSyncedWithCloud;
+
+  DbIncome({
+    required this.id,
+    required this.price,
+    required this.note,
+    required this.tag,
+    required this.date,
+    required this.time,
+    required this.userId,
+    required this.isSyncedWithCloud,
+  });
+
+  DbIncome.fromRow(Map<String, Object?> map)
       : id = map[idColmn] as int,
         price = map[priceColmn] as double,
         note = map[noteColmn] as String,
@@ -143,7 +523,7 @@ class DatabaseCunehat {
   }
 
   @override
-  operator ==(covariant DatabaseCunehat other) {
+  operator ==(covariant DbExpense other) {
     return id == other.id;
   }
 
--- lib/services/crud/database_exceptions.dart
+++ lib/services/crud/database_exceptions.dart
@@ -1,3 +1,20 @@
+// database exception
 class DatabaseAlreadyOpenException implements Exception {}
 
+class DatabaseIsNotOpen implements Exception {}
+
 class UnableToGetDirectoryException implements Exception {}
+
+// user exception
+class UserAlreadyExists implements Exception {}
+
+class CouldNotFindUser implements Exception {}
+
+class CouldNotDeleteUser implements Exception {}
+
+// expense and income exception ()
+class CouldNotDeleteData implements Exception {}
+
+class CouldNotFindData implements Exception {}
+
+class CouldNotUpdateData implements Exception {}
