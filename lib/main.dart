cff3fbb35975e4af28a912b485a5ea1879e2dfaa
halil ibrahim avsar <hllbrhmavsar@gmail.com>
2023-12-11 23:02:27 +0300

    fixed some of typo and commented(deactivated) unused flutter libraries

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,5 @@
 // TODO : if user signed in via google, then cant be log in using email-password.
 
-import 'dart:async';
-
-import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:cunehat/constants/routes.dart';
 import 'package:cunehat/constants/current_month_range.dart';
 import 'package:cunehat/services/firestore/firestore_service.dart';
@@ -16,7 +13,6 @@ import 'package:cunehat/views/login_views/login_screen.dart';
 import 'package:cunehat/views/login_views/register_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 
 import 'package:intl/date_symbol_data_local.dart';
@@ -75,85 +71,85 @@ void main() async {
   );
 }
 
-class CheckConnection extends StatefulWidget {
-  const CheckConnection({super.key});
+// class CheckConnection extends StatefulWidget {
+//   const CheckConnection({super.key});
 
-  @override
-  State<CheckConnection> createState() => _CheckConnectionState();
-}
+//   @override
+//   State<CheckConnection> createState() => _CheckConnectionState();
+// }
 
-class _CheckConnectionState extends State<CheckConnection> {
-  ConnectivityResult _connectionStatus = ConnectivityResult.none;
-  final Connectivity _connectivity = Connectivity();
-  late StreamSubscription<ConnectivityResult> _connectivitySubscription;
+// class _CheckConnectionState extends State<CheckConnection> {
+//   ConnectivityResult _connectionStatus = ConnectivityResult.none;
+//   final Connectivity _connectivity = Connectivity();
+//   late StreamSubscription<ConnectivityResult> _connectivitySubscription;
 
-  @override
-  void initState() {
-    super.initState();
-    initConnectivity();
+//   @override
+//   void initState() {
+//     super.initState();
+//     initConnectivity();
 
-    _connectivitySubscription =
-        _connectivity.onConnectivityChanged.listen(_updateConnectionStatus);
-  }
+//     _connectivitySubscription =
+//         _connectivity.onConnectivityChanged.listen(_updateConnectionStatus);
+//   }
 
-  @override
-  void dispose() {
-    _connectivitySubscription.cancel();
-    super.dispose();
-  }
+//   @override
+//   void dispose() {
+//     _connectivitySubscription.cancel();
+//     super.dispose();
+//   }
 
-  @override
-  Widget build(BuildContext context) {
-    return StreamBuilder(
-      stream: Connectivity().onConnectivityChanged,
-      builder: (BuildContext context, AsyncSnapshot snapshot) {
-        return FutureBuilder(
-          future: initConnectivity(),
-          builder: (BuildContext context, AsyncSnapshot snapshot) {
-            if (_connectionStatus == ConnectivityResult.wifi ||
-                _connectionStatus == ConnectivityResult.mobile) {
-              return const LoginScreen();
-            } else {
-              return const Dialog.fullscreen(
-                backgroundColor: Colors.redAccent,
-                child: Center(
-                  child: Text(
-                    "There is no internet connection!\n\nPlease connect wifi or mobile network",
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontStyle: FontStyle.italic,
-                      fontSize: 28,
-                    ),
-                  ),
-                ),
-              );
-            }
-          },
-        );
-      },
-    );
-  }
+//   @override
+//   Widget build(BuildContext context) {
+//     return StreamBuilder(
+//       stream: Connectivity().onConnectivityChanged,
+//       builder: (BuildContext context, AsyncSnapshot snapshot) {
+//         return FutureBuilder(
+//           future: initConnectivity(),
+//           builder: (BuildContext context, AsyncSnapshot snapshot) {
+//             if (_connectionStatus == ConnectivityResult.wifi ||
+//                 _connectionStatus == ConnectivityResult.mobile) {
+//               return const LoginScreen();
+//             } else {
+//               return const Dialog.fullscreen(
+//                 backgroundColor: Colors.redAccent,
+//                 child: Center(
+//                   child: Text(
+//                     "There is no internet connection!\n\nPlease connect wifi or mobile network",
+//                     textAlign: TextAlign.center,
+//                     style: TextStyle(
+//                       fontWeight: FontWeight.bold,
+//                       fontStyle: FontStyle.italic,
+//                       fontSize: 28,
+//                     ),
+//                   ),
+//                 ),
+//               );
+//             }
+//           },
+//         );
+//       },
+//     );
+//   }
 
-  Future<void> initConnectivity() async {
-    late ConnectivityResult result;
-    // Platform messages may fail, so we use a try/catch PlatformException.
-    try {
-      result = await _connectivity.checkConnectivity();
-    } on PlatformException {
-      return;
-    }
+//   Future<void> initConnectivity() async {
+//     late ConnectivityResult result;
+//     // Platform messages may fail, so we use a try/catch PlatformException.
+//     try {
+//       result = await _connectivity.checkConnectivity();
+//     } on PlatformException {
+//       return;
+//     }
 
-    if (!mounted) {
-      return Future.value(null);
-    }
+//     if (!mounted) {
+//       return Future.value(null);
+//     }
 
-    return _updateConnectionStatus(result);
-  }
+//     return _updateConnectionStatus(result);
+//   }
 
-  Future<void> _updateConnectionStatus(ConnectivityResult result) async {
-    setState(() {
-      _connectionStatus = result;
-    });
-  }
-}
+//   Future<void> _updateConnectionStatus(ConnectivityResult result) async {
+//     setState(() {
+//       _connectionStatus = result;
+//     });
+//   }
+// }
---
+++ lib/theme/app_colors.dart
@@ -0,0 +1,89 @@
+// import 'package:flutter_neumorphic/flutter_neumorphic.dart';
+
+// /// -----WHITE THEME-------
+// class BlackThemeColors {
+//   /// login colors
+//   static Color loginBackground = Colors.blueGrey.shade50;
+//   static Color loginButton = Colors.blueGrey.shade50;
+//   static Color loginText = Colors.blueGrey.shade50;
+
+//   /// main colors
+//   static Color appbar = Colors.blueGrey.shade400;
+//   static Color appbarButton = Colors.blueGrey.shade400;
+//   static Color appbarText = Colors.blueGrey.shade400;
+
+//   static Color body = Colors.blueGrey.shade50;
+//   static Color bodyButton = Colors.blueGrey.shade50;
+//   static Color bodyText = Colors.blueGrey.shade50;
+//   static Color navigator = Colors.blueGrey.shade50;
+//   static Color navigatorButton = Colors.blueGrey.shade50;
+//   static Color navigatorText = Colors.blueGrey.shade50;
+
+//   /// home colors
+//   static Color homeIncome = Colors.green.shade50;
+//   static Color homeIncomeText = Colors.green;
+//   static Color homeExpense = Colors.red.shade50;
+//   static Color homeExpenseText = Colors.red;
+
+//   /// ıncome colors
+//   static Color income = Colors.green.shade300;
+
+//   /// expense colors
+//   static Color expense = Colors.red.shade300;
+
+//   /// success colors
+//   static Color succes = Colors.green;
+
+//   /// info colors
+//   static Color info = Colors.blue;
+
+//   /// warning colors
+//   static Color warning = Colors.yellow;
+
+//   /// error colors
+//   static Color error = Colors.red;
+// }
+
+// /// ------BLACK THEME------
+
+// class WhiteThemeColors {
+//   /// login colors
+//   static Color loginBackground = Colors.blueGrey.shade50;
+//   static Color loginButton = Colors.blueGrey.shade50;
+//   static Color loginText = Colors.black;
+
+//   /// main colors
+//   static Color appbar = Colors.blueGrey.shade100;
+//   static Color appbarButton = Colors.blueGrey.shade100;
+//   static Color appbarText = Colors.blueGrey.shade900;
+//   static Color body = Colors.blueGrey.shade50;
+//   static Color bodyButton = Colors.blueGrey.shade50;
+//   static Color bodyText = Colors.black;
+//   static Color navigator = Colors.blueGrey.shade50;
+//   static Color navigatorButton = Colors.blueGrey.shade50;
+//   static Color navigatorText = Colors.blueGrey.shade50;
+
+//   /// home colors
+//   static Color homeIncome = Colors.green.shade50;
+//   static Color homeIncomeText = Colors.green;
+//   static Color homeExpense = Colors.red.shade50;
+//   static Color homeExpenseText = Colors.red;
+
+//   /// ıncome colors
+//   static Color income = Colors.green;
+
+//   /// expense colors
+//   static Color expense = Colors.red;
+
+//   /// success colors
+//   static Color succes = Colors.green;
+
+//   /// info colors
+//   static Color info = Colors.blue;
+
+//   /// warning colors
+//   static Color warning = Colors.yellow;
+
+//   /// error colors
+//   static Color error = Colors.red;
+// }
