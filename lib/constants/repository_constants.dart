c14b5a7d71419351cb3f1ded60afc32cefac0055
halil ibrahim avsar <hllbrhmavsar@gmail.com>
2025-11-17 01:25:48 +0300

    Revise code changes

---
+++ lib/constants/repository_constants.dart
@@ -0,0 +1,15 @@
+// collections (tables)
+// const String expenseTable = 'expenses';
+// const String incomeTable = 'incomes';
+// const String userIdColmn = 'userId';
+
+//documents (rows)[not availabel]
+
+// fields (Every row contains map, field is map key)
+const fieldUserId = "userId";
+const fieldTitle = "title";
+const fieldTag = "tag";
+const fieldAmount = "amount";
+const fieldDate = "date";
+const fieldTime = "time";
+const fieldBalance = "balance";
